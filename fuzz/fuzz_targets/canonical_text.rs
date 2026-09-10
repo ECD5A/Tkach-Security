7d555cb38a13b2a8bda38bac98db855d2c931413
ECD5A <111098024+ECD5A@users.noreply.github.com>
2026-09-10 06:05:40 +0500

    ci: identify fuzz targets with project headers

--- fuzz/fuzz_targets/canonical_text.rs
+++ fuzz/fuzz_targets/canonical_text.rs
@@ -1,3 +1,14 @@
+/*
+ * Tkach Security
+ *
+ * Copyright 2026 ECD5A
+ * Licensed under the Apache License, Version 2.0.
+ *
+ * Repository: https://github.com/ECD5A/Tkach-Security
+ *
+ * See LICENSE and SECURITY.md.
+ */
+
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
